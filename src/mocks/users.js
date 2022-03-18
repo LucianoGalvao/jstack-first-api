e7e5ba2a35fd13bf6b1ce02316c60ea3d0df986d
Luciano Galvão <luciano_sdmf@hotmail.com>
2022-03-17 21:19:20 -0300

    Commit do módulo completo

---
+++ src/mocks/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        id: 1,
+        name: "Luciano"
+    },
+    {
+        id: 2,
+        name: "Patricia"
+    },
+    {
+        id: 3,
+        name: "João"
+    }
+]
